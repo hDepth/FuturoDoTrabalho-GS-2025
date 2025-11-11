1f4a3a863f26a412a27defc93cafbdadf0d5e192
hDepth <dephenriquejp@gmail.com>
2025-11-11 17:14:34 -0300

    Estruturacao base

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>App está rodando!</Text>
     </View>
   );
 }
@@ -17,4 +16,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
 });
