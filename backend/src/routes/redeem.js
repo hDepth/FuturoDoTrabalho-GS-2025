c65818c4576eb187c495e3dbbce562fb1188554d
hDepth <dephenriquejp@gmail.com>
2025-11-23 00:34:00 -0300

    Backend no render

--- backend/src/routes/redeem.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const redeem = require('../controllers/redeemController');
-const auth = require('../middlewares/auth');
-router.post('/:id/redeem', auth, redeem.redeem);
-module.exports = router;
